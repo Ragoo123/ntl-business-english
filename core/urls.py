f3d5e00c6d0b0c098440d869fb2f57e644e40f27
Ragoo123 <157330891+Ragoo123@users.noreply.github.com>
2025-12-05 20:30:04 +0700

    index, login and register layout finished

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 #urls.py
 from django.urls import path
-from .views import index
+from . import views as core_views
+
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', core_views.indexView.as_view(), name='index'),
+    path('login/', core_views.loginView.as_view(), name='login'),
+    path('register/', core_views.registerView.as_view(), name='register'),
 ]
