9f45cd321dede0ce46503becd1f36d3e41be5791
Ragoo123 <157330891+Ragoo123@users.noreply.github.com>
2025-12-08 11:24:15 +0700

    folders db filled and auto admin owner

--- vocabulary/admin.py
+++ vocabulary/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Folder, VocabularyWord, QuizScore
 
-admin.site.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created_at')
+    exclude = ('owner',)  # hide owner field in form
+
+    def save_model(self, request, obj, form, change):
+        if not obj.owner:
+            obj.owner = User.objects.get(username='mo')  # Mo is assigned automatically
+        super().save_model(request, obj, form, change)
+
+        
+admin.site.register(Folder, FolderAdmin)
 admin.site.register(VocabularyWord)
 admin.site.register(QuizScore)
--- vocabulary/models.py
+++ vocabulary/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # ---------------------------
 class Folder(models.Model):
     name = models.CharField(max_length=255)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='folders')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='folders', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
