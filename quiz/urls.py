37d2ea727c7beedf8da9610a041537f434b76189
Ragoo123 <157330891+Ragoo123@users.noreply.github.com>
2025-12-14 01:18:49 +0700

    result page implemented and displays correct data, next, review questions modal

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,8 +1,9 @@
 #urls.py
 from django.urls import path
-from .views import quizView, checkAnswer
+from .views import quizView, checkAnswer, nextQuestion
 
 urlpatterns = [
     path('quiz/<int:folder_id>/', quizView, name='quiz'),
     path('quiz/check/', checkAnswer, name='check_answer'),
+    path('quiz/next/', nextQuestion, name='load_next_question'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -100,3 +100,37 @@ def checkAnswer(request):
     }
 
     return render(request, "partials/feedback_quiz_question.html", context)
+
+def nextQuestion(request):
+    #retrieve quiz data from session
+    quiz_data = request.session.get('quiz_data', [])
+    quiz_index = request.session.get('quiz_index', 0) + 1  #move to next question
+    request.session['quiz_index'] = quiz_index
+    request.session.modified = True
+
+    folder_id = request.session.get('folder_id')
+    folder_name = request.session.get('folder_name')
+
+    if quiz_index >= len(quiz_data):
+        #quiz is over
+        context = {
+            "quiz_score": request.session.get('quiz_score', 0),
+            "total_questions": len(quiz_data),
+            "folder_id": folder_id,
+            "folder_name": folder_name,
+            'percent': int((request.session.get('quiz_score', 0) / len(quiz_data)) * 100) if len(quiz_data) > 0 else 0,
+            'incorrect': len(quiz_data) - request.session.get('quiz_score', 0),
+        }
+        return render(request, "quiz/quiz_finished.html", context)
+    else:
+        #next question
+        current_question = quiz_data[quiz_index]
+        context = {
+            "current_question": current_question,
+            "quiz_index": quiz_index + 1,
+            "total_questions": len(quiz_data),
+            "quiz_score": request.session.get('quiz_score', 0),
+            "folder_id": folder_id,
+            "folder_name": folder_name,
+        }
+        return render(request, "partials/_quiz_question.html", context)
