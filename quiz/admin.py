4b96d11d026e3c59c33be8d3a185eadde9e647e4
Ragoo123 <157330891+Ragoo123@users.noreply.github.com>
2025-12-19 12:24:30 +0700

    reading model and data ready, time to design and pass into read pages

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import ReadingText, ReadingQuestion, ReadingOption
 
-# Register your models here.
+
+@admin.register(ReadingText)
+class ReadingTextAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text_title', 'folder', 'created_at')
+    search_fields = ('text',)
+
+
+@admin.register(ReadingQuestion)
+class ReadingQuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'reading', 'question_text')
+    search_fields = ('question_text',)
+
+
+@admin.register(ReadingOption)
+class ReadingOptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'get_reading_title',
+        'question',
+        'option_text',
+        'is_correct',
+    )
+    list_filter = ('is_correct',)
+
+    def get_reading_title(self, obj):
+        return obj.question.reading.text_title
+
+    get_reading_title.short_description = "Reading Title"
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from vocabulary.models import Folder
+
+class ReadingText(models.Model):
+    folder = models.ForeignKey(
+        Folder,
+        on_delete=models.CASCADE,
+        related_name='reading_texts'
+    )
+    text_title = models.CharField(max_length=255)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text_title
+
+    
+
+class ReadingQuestion(models.Model):
+    reading = models.ForeignKey(
+        ReadingText,
+        on_delete=models.CASCADE,
+        related_name='questions'
+    )
+    question_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question_text
+    
+class ReadingOption(models.Model):
+    question = models.ForeignKey(
+        ReadingQuestion,
+        on_delete=models.CASCADE,
+        related_name='options'
+    )
+    option_text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        status = "✓" if self.is_correct else "✗"
+        return f"{status} {self.option_text}"
 
 # Create your models here.
